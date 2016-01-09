85933b95b448c9c0b81f3178f9c6b0c131e724e1
Nicholas Yang <nick@nicholasyang.com>
2016-01-09 16:55:23 -0500

    final touches

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+@application.route('/')
+@application.route('/home')
 def home():
 	return "<h1> Hi </h1>"
 
 
 
 if __name__ == "__main__":
-    app.debug == True
-    app.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
-    app.run('107.170.107.124',port=8000)
+    application.debug == True
+    application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
+    application.run('107.170.107.124',port=8000)
