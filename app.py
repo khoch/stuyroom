1956b313aa40c87586c10ae601c97a93abe87840
Nicholas Yang <nick@nicholasyang.com>
2016-01-09 17:31:01 -0600

    reorganized stuff in flask format. If we need to turn it back, I will do it

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 application = Flask(__name__)
 
 @application.route('/')
 @application.route('/home')
 def home():
-	return "<h1> Hi </h1>"
+	return '''
+<h1> Hello World </h1>
+<a href="/cal"> Calendar </a>
 
+<a href="/test"> Test </a> 
+'''
 
+@application.route('/cal')
+def cal():
+    return render_template("templates/cal.html")
+
+@application.route('/test')
+def test():
+    return render_template("templates/test.html")
 
 if __name__ == "__main__":
     application.debug == True
     application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
-    application.run('107.170.107.124',port=8000)
+    application.run('0.0.0.0',port=8000)
--- static/js/cal.js
+++ static/js/cal.js
@@ -33,35 +33,27 @@ var fillCalendar = function fillCalendar(year, month){
     //edits html table
     index = 0;
     $(".cal").each(function(){
-	console.log(month);
     	$(this).find('td').each(function(){
-    	    var thisDay = dates[index];
-		//get rid of days in previous month
-	    $(this).on("click", function(e){
-		$("#availrooms").empty();
-		$("#unavailrooms").empty();	
-		$(".active").toggleClass('active');
-		$(this).find('a').toggleClass('active');	
-		$(this).toggleClass('active');
-	
-	
-	    });
-    	    if ((index <= 7 && thisDay > 20) ||
-    		(index >= 28 && thisDay < 20)
+    	    var d = dates[index];
+    	    if ((index <= 7 && d > 20) ||
+    		(index >= 28 && d < 20)
 		){
+		//get rid of days in previous month
     		$(this).toggleClass("off",true);
-		//get rid of days that have passed
     	    } else if (year < date.getFullYear()){
 		$(this).toggleClass("off",true);
-	    } else if (month < date.getMonth() && year == date.getFullYear()){
+	    } else if (month <= date.getMonth() && year == date.getFullYear()){
+		//get rid of days that have passed
 		$(this).toggleClass("off",true);
+		if (month == date.getMonth()){
+		    if( d >= date.getDate()){
+			$(this).toggleClass("off",false);
+		    }
+		}
 	    }else {
     		$(this).toggleClass("off",false);
-		$(this).on("click", function(e){
-		   calEvent(thisDay,month,year);
-		});
     	    }
-    	    $(this).find('a').text(thisDay);
+    	    $(this).find('a').text(d);
     	    index++;
     	})
     });
@@ -71,18 +63,6 @@ var date = new Date();
 currentM = date.getMonth();
 currentY = date.getFullYear();
 fillCalendar(currentY, currentM);
-
-var calEvent = function calEvent(day, month, year){
-    var rooms = availableRooms(day, month, year);
-    for (var i = 0; i < rooms.length;i++){
-	$("#availrooms").append('<li><a href="room.html?rm=' + rooms[i]+ '">'
-				+ rooms[i] + '</a></li>');
-    }
-    var takenrooms = unavailableRooms(day, month, year);
-    for (var i = 0; i < takenrooms.length; i++){
-	$("#unavailrooms").append('<li>' + takenrooms[i][0] + ' : ' + takenrooms[i][1] + '</li>');
-    }
-}
 
 var nextMonth = function(e){
     currentM++;
@@ -114,8 +94,7 @@ var availableRooms = function availableRooms(month, day, year){
 }
 
 var unavailableRooms = function unavailableRooms(month, day, year){
-    //returns 2D array with taken rooms and club name
-    return [[555, "smash bros"],[555, "jsa"],[555, "key club"],[555,"history club"]]
+    //returns list of taken rooms
 }
 
 
