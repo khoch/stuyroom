752a1449d371b26fdf094496a419cc6230be4d92
Nicholas Yang <nick@nicholasyang.com>
2016-01-10 13:55:25 -0600

    fixed minor error

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for, escape
+
 
 application = Flask(__name__)
 
 @application.route('/')
 @application.route('/home')
 def home():
-	return '''
-<h1> Hello World </h1>
-<a href="/cal"> Calendar </a>
-
-<a href="/test"> Test </a> 
-'''
+	return render_template("home.html")
 
 @application.route('/cal')
 def cal():
-    return render_template("templates/cal.html")
+    return render_template("cal.html")
 
 @application.route('/test')
 def test():
-    return render_template("templates/test.html")
+    return render_template("rotest.html")
 
 if __name__ == "__main__":
-    application.debug == True
-    application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
+    application.debug = True
+   # application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
     application.run('0.0.0.0',port=8000)
