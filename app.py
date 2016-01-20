0046177bf7dfeb0e98807346557df13d24a61b6d
Nicholas Yang <nick@nicholasyang.com>
2016-01-20 12:41:44 -0500

    fixed err

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for, es
+from flask import Flask, render_template, request, session, redirect, url_for
 
 
 application = Flask(__name__)
