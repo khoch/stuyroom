2c965760201f4b2cbed7b5b6c8397176586783b9
Daisy B <daisybarbanel@yahoo.com>
2016-01-25 13:18:54 -0500

    fixed link to admin

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import database
 application = Flask(__name__)
 
 @application.route('/')
-@application.route('/home')
 @application.route('/cal')
 def cal():
+    #check if user is logged in
+    if "loggedin" not in session:
+        session["loggedin"] = False;
+    #elif session["loggedin"] == True:
+     #   return render_template("cal.html", logged_in = loggedin)
     return render_template("cal.html")
 
 @application.route('/reserve', methods=['GET','POST'])
@@ -45,7 +49,84 @@ def available():
         data = database.getAvailableRooms(date)
     return json.dumps(data)
 
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    if "loggedin" not in session:
+        session["loggedin"] = False
+    if session["loggedin"]:
+        return redirect(url_for("home"))
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['user']
+        passw = request.form['pass']
+        #button = request.form['button']
+	if database.checkUser(uname, passw) == 0:
+            session["loggedin"] = True
+            session['user'] = uname
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", NOTLOGGEDIN = "Error: Wrong username or password.")  
+
+
+@application.route("/changepass", methods=['GET', 'POST'])
+def changepass():
+    if "loggedin" not in session:
+        session["loggedin"] = False
+    if request.method=="GET":
+        return render_template("changepass.html")
+    else:
+        uname = request.form['user']
+        oldpassw = request.form['oldpass']
+        newpassw1 = request.form['newpass1']
+        newpassw2 = request.form['newpass2']
+        #button = request.form['button']
+        if database.checkUser(uname, oldpassw) == 0:
+            if newpassw1 != newpassw2:
+                return render_template("changepass.html", ERROR = "Error: New passwords do not match.") 
+            database.changePassword(uname, newpassw1)
+            return redirect(url_for("login"))
+        else:
+            return render_template("changepass.html", ERROR = "Error: Wrong username or password.")
+    
+
+@application.route("/home", methods=["GET","POST"])
+def home():
+	return render_template("home.html")
+
+@application.route("/logout")
+def logout():
+    session["loggedin"] = False
+    session['uname'] = ""
+    return redirect(url_for("home"))
+
+@application.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method=="GET":
+        return render_template("signup.html")
+    else:
+        if request.form['pass'] != request.form['confirmpass']:
+            return render_template("signup.html", NOTLOGGEDIN = "Error: 'Password' and 'Confirm Password' do not match.")
+        elif len(request.form['user']) < 4 or len(request.form['pass']) < 8:
+            return render_template("signup.html", NOTLOGGEDIN = "Error: 'Username' must be at least 4 characters and 'Password' must be at least 8 characters.")
+        else: 
+            uname = request.form['user']
+            passw = request.form['pass']
+            if database.addUser(uname, passw):
+                return redirect(url_for("login"))
+            else:
+                return render_template("signup.html", NOTLOGGEDIN = "Error: Username already exists")
+
+@application.route("/myaccount")
+def myaccount():
+    if 'loggedin' in session and 'user' in session and session["loggedin"]:
+		return render_template("myaccount.html", LOGGEDIN = session['user'])
+    else:
+		return(render_template("myaccount.html"))
+
+application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
 if __name__ == "__main__":
-    application.debug = True
-    # application.secret_key = "k|5D5PH~21x1.zLg1r.Gj?9v"
-    application.run('0.0.0.0',port=8000)
+    
+
+    application.run('0.0.0.0',port=8001)
